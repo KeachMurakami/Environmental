20210985b652509442e520ef131f3f9265697e02
MurakamiKeach <m.keach.11@gmail.com>
2016-01-24 19:32:41 +0900

    Fin
    一応完成。
    Rmd生成からknitまで実装できればなおよいが、ペンディング。

--- EnvLogs/EnvLog.R
+++ EnvLogs/EnvLog.R
@@ -1,77 +1,135 @@
-### テスト運転中
-setwd("~/GitHub/BeeLabR/Environmental/EnvLogs/")
-files <- dir(pattern = "xlsx")[1]
-###
-
-
-
-ExptInfo <-
-  read.xlsx(files, 1, startRow = 2, stringsAsFactors = FALSE) %>%
-  na.omit
-
-Hist_ls <- as.list(numeric(nrow(ExptInfo)))
-Hourly_ls <- as.list(numeric(nrow(ExptInfo)))
-Daily_ls <- as.list(numeric(nrow(ExptInfo)))
-
-
-histor <- function(df, value) 
-  df %>%
-  mutate(DayNight = as.factor(DayNight)) %>%
-  ggplot(aes_string(x = value, fill = "DayNight")) +
-  geom_histogram(alpha = .25)
-
-lapply(1:nrow(ExptInfo), function(X) {
-  ExptInfo %>%
-    slice(X) %>%
-    {
-      temps <-
-        readGL(ID = .$GL_id, ch = .$GL_ch,
-               StartDay = .$StartDay, StartTime = .$StartTime,
-               EndDay = .$EndDay, EndTime = .$EndTime,
-               ONtime = .$LightOn, OFFtime = .$LightOff) 
-      
-      RH_CO2s <-    
-        readMCH(ID = .$MCH_id,
-                StartDay = .$StartDay, StartTime = .$StartTime,
-                EndDay = .$EndDay, EndTime = .$EndTime,
-                ONtime = .$LightOn, OFFtime = .$LightOff)
-      
-      Hist_ls[[X]] <<-
-        list(histor(temps$Raw, "Temp"), histor(RH_CO2s$Raw, "RH"), histor(RH_CO2s$Raw, "CO2"))
-      
-      
-      temps$Hourly %<>%
-        rename(GL_ID = ID, GL_CH = ch)
-      RH_CO2s$Hourly %<>%
-        rename(MCH_ID = ID)
-      Hourly_ls[[X]] <<-
-        full_join(temps$Hourly, RH_CO2s$Hourly, by = c("DayNight", "Day", "Hour", "DayHour", "Time"))
-
-      temps$Daily %<>%
-        rename(GL_ID = ID, GL_CH = ch)
-      RH_CO2s$Daily %<>%
-        rename(MCH_ID = ID)
-      Daily_ls[[X]] <<-
-        full_join(temps$Daily, RH_CO2s$Daily, by = c("DayNight", "Day", "Time"))
+EnvLog <-
+  function(files, by_cham_facet = F){
+    
+    if(missing(files)) files <- file.choose()
+    
+    ExptInfo <-
+      read.xlsx(files, 1, startRow = 2, stringsAsFactors = FALSE) %>%
+      na.omit
+    
+    #### data input  #####
+    Hist_ls <- as.list(numeric(nrow(ExptInfo)))
+    Hourly_ls <- as.list(numeric(nrow(ExptInfo)))
+    Daily_ls <- as.list(numeric(nrow(ExptInfo)))
+    Span_ls <- as.list(numeric(nrow(ExptInfo)))
+    
+    histor <- function(df, value) {
+      df %>%
+        mutate(DayNight = as.factor(DayNight)) %>%
+        ggplot(aes_string(x = value, fill = "DayNight")) +
+        geom_histogram(alpha = .25) +
+        ggtitle(label = unique(df$cham))
     }
-})
+    
+    
+    lapply(1:nrow(ExptInfo), function(X) {
+      
+      get_cham <- function(df){
+        if(is.data.frame(df)) {
+          mutate(df, cham = ExptInfo$ChamberID[X])
+        } else {
+          df
+        }
+      }
+      
+      ExptInfo %>%
+        slice(X) %>%
+        {
+          temps <-
+            readGL(ID = .$GL_id, ch = .$GL_ch,
+                   StartDay = .$StartDay, StartTime = .$StartTime,
+                   EndDay = .$EndDay, EndTime = .$EndTime,
+                   ONtime = .$LightOn, OFFtime = .$LightOff) %>%
+            llply(get_cham)
+          
+          RH_CO2s <-    
+            readMCH(ID = .$MCH_id,
+                    StartDay = .$StartDay, StartTime = .$StartTime,
+                    EndDay = .$EndDay, EndTime = .$EndTime,
+                    ONtime = .$LightOn, OFFtime = .$LightOff) %>%
+            llply(get_cham)
+          
+          Hist_ls[[X]] <<-
+            list(histor(temps$Raw, "Temp"), histor(RH_CO2s$Raw, "RH"), histor(RH_CO2s$Raw, "CO2"))
+          
+          
+          temps$Hourly %<>%
+            rename(GL_ID = ID, GL_CH = ch)
+          RH_CO2s$Hourly %<>%
+            rename(MCH_ID = ID, MeanTempMCH = MeanTemp, SDTempMCH = SDTemp)
+          Hourly_ls[[X]] <<-
+            full_join(temps$Hourly, RH_CO2s$Hourly, by = c("DayNight", "Day", "Hour", "DayHour", "Time", "cham"))
+          
+          temps$Daily %<>%
+            rename(GL_ID = ID, GL_CH = ch)
+          RH_CO2s$Daily %<>%
+            rename(MCH_ID = ID, MeanTempMCH = MeanTemp, SDTempMCH = SDTemp)
+          Daily_ls[[X]] <<-
+            full_join(temps$Daily, RH_CO2s$Daily, by = c("DayNight", "Day", "Time", "cham"))
+          
+          temps$Span %<>%
+            rename(GL_ID = ID, GL_CH = ch)
+          RH_CO2s$Span %<>%
+            rename(MCH_ID = ID, MeanTempMCH = MeanTemp, SDTempMCH = SDTemp)
+          Span_ls[[X]] <<-
+            full_join(temps$Span, RH_CO2s$Span, by = c("DayNight", "cham"))
+          
+        }
+    })
+    
+    #### data input fin #####
+    
+    #### data visualize ####
 
-#### input fin #####
+    PlotHourly <-
+      function(by_cham = by_cham_facet){
+        lapply(c("Temp", "RH", "CO2"), function(variables){
+          fig1 <-
+            Hourly_ls %>%
+            rbind_all2 %>%
+            select(cham, Time, DayNight, GL_ID, GL_CH, MCH_ID, v = ends_with(variables)) %>%
+            ggplot(aes(x = Time, y = v1, fill = cham)) +
+            geom_ribbon(aes(ymin = v1 - v2, ymax = v1 + v2), alpha = .1) +
+            geom_point(aes(col = cham), alpha = .5) +
+            ggtitle(variables)
+          
+          if(by_cham) {
+            fig1 + facet_grid(cham ~ .)
+          } else {
+            fig1
+          }
+        })
+      }
+    
+    PlotDaily <-
+      function(by_cham = by_cham_facet){
+        lapply(c("Temp", "RH", "CO2"), function(variables){
+          fig2 <-
+            Daily_ls %>%
+            rbind_all2 %>%
+            select(cham, Time, DayNight, GL_ID, GL_CH, MCH_ID, v = ends_with(variables)) %>%
+            ggplot(aes(x = Time, y = v1, col = cham)) +
+            geom_errorbar(aes(ymin = v1 - v2, ymax = v1 + v2), alpha = .1) +
+            geom_point(alpha = .5) +
+            ggtitle(variables)
+          
+          if(by_cham) {
+            fig2 + facet_grid(cham ~ .)
+          } else {
+            fig2
+          }
+        })
+      }
+    
+    StatSpan <-
+      Span_ls %>%
+      rbind_all2
+    
+    list(HourlyTimeCourse = PlotHourly(),
+         DailyTimeCourse = PlotDaily(),
+         SpanHistograms = Hist_ls,
+         SpanData = StatSpan,
+         Info = ExptInfo) %>%
+      return
+  }
 
-#### data handling ####
-
-# 先に場所情報を乗っける？
-chamset <- function(GL_ID, GL_CH) {
-  ExptInfo %>%
-    filter(GL_id == GL_ID, GL_ch == GL_CH) %>%
-    .$ChamberID
-}
-
-
-Hourly_ls %>%
-  rbind_all %>%
-  mutate(cham = Vectorize(chamset)(GL_ID, GL_CH)) %>%
-  melt2(id.vars = )
-  ggplot(aes(x = Time, y = MeanTemp.x, col = ID_CH)) +
-  geom_ribbon(aes(ymin = MeanTemp.x - SDTemp.x, ymax = MeanTemp.x + SDTemp.x, fill = ID_CH), alpha = .1) +
-  geom_line()
